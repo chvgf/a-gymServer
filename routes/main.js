788c36a7777f799e7ee03baf371271147ffc94cd
chvgf <chvgf@naver.com>
2024-06-30 02:55:55 +0900

    More

--- routes/main.js
+++ routes/main.js
@@ -8,9 +8,10 @@ const router = express.Router();
 
 router.get("/content", async (req, res) => {
   try {
-    const data = await db.collection("agymTest").find({}).toArray();
+    const data = await db.collection("agymTest").find({}).limit(8).toArray();
+    const dataSkip = await db.collection("agymTest").find({}).skip(data.length).limit(4).toArray();
     res.json({
-      data: data,
+      data: [data, dataSkip],
       flag: true,
       message: "테스트게시글",
     });
